4e439f47e3b7a1b6d56e83f63b5bf0d9d24f3003
Timothy Pruitt <TAPruitt2@student.fullsail.edu>
2024-09-05 19:21:18 -0400

    Updated the nav to be attatched to the rest of the levels and logic working.

--- app/src/main/java/com/example/wordworld/LevelTwoActivity.java
+++ app/src/main/java/com/example/wordworld/LevelTwoActivity.java
@@ -1,24 +1,71 @@
 package com.example.wordworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
 public class LevelTwoActivity extends AppCompatActivity {
 
     private Button submitButton;
-
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_level_two);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                Intent intent;
+
+                if (id == R.id.level_one) {
+                    intent = new Intent(LevelTwoActivity.this, LevelOneActivity.class);
+                    startActivity(intent);
+                } else if (id == R.id.level_two) {
+                    intent = new Intent(LevelTwoActivity.this, LevelTwoActivity.class);
+                    startActivity(intent);
+                } else if (id == R.id.level_three) {
+                    intent = new Intent(LevelTwoActivity.this, LevelThreeActivity.class);
+                    startActivity(intent);
+                } else if (id == R.id.badges) {
+                    // Navigate to Badges activity
+                } else if (id == R.id.profile) {
+                    // Navigate to Profile activity
+                } else if (id == R.id.leader_board) {
+                    // Navigate to Leaderboard activity
+                }
+
+                drawerLayout.closeDrawers(); // Close the drawer after an item is clicked
+                return true;
+            }
+        });
+
+        ImageButton navButton = findViewById(R.id.nav_button);
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerOpen(navigationView)) {
+                    drawerLayout.closeDrawer(navigationView);
+                } else {
+                    drawerLayout.openDrawer(navigationView);
+                }
+            }
+        });
         // Initialize the back button and set the click listener
         ImageButton backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
