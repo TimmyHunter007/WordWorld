8f8188ce4dc3d27b41c047b77ce4e331356818ad
Timothy Pruitt <TAPruitt2@student.fullsail.edu>
2024-09-19 21:05:01 -0400

    Badge logic with commeneted out code thats giving me shut down issues But in progress

--- app/src/main/java/com/example/wordworld/BadgesActivity.java
+++ app/src/main/java/com/example/wordworld/BadgesActivity.java
@@ -6,26 +6,94 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ContentLoadingProgressBar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 
 public class BadgesActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private ProgressBar progressBar;
+    //
+    private ProgressBar progressBar1;
+    private int progress1 = 0;
+    private final int MaxProgressBar1 = 1;
+    //
+    private ProgressBar progressBar2;
+    private int progress2 = 0;
+    private final int MaxProgressBar2 = 1;
+    //
+    private ProgressBar progressBar3;
+    private int progress3 = 0;
+    private final int MaxProgressBar3 = 1;
+    //
+    private ProgressBar progressBar4;
+    private int progress4 = 0;
+    private final int MaxProgressBar4 = 1;
+    //
+    private ProgressBar progressBar5;
+    private int progress5 = 0;
+    private final int MaxProgressBar5 = 10;
+    //
+    private ProgressBar progressBar6;
+    private int progress6 = 0;
+    private final int MaxProgressBar6 = 20;
+    //
+    private ProgressBar progressBar7;
+    private int progress7 = 0;
+    private final int MaxProgressBar7 = 50;
+    //
+    private ProgressBar progressBar8;
+    private int progress8 = 0;
+    private final int MaxProgressBar8 = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         getSupportActionBar().hide();
         setContentView(R.layout.activity_badges);
 
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        /*
+        progressBar1 = findViewById(R.id.progressBar);
+        progressBar1.setMax(MaxProgressBar1);
+        progressBar1.setProgress(progress1);
 
-        CompleteWord();
+        progressBar2 = findViewById(R.id.ProgressBar2);
+        progressBar2.setMax(MaxProgressBar2);
+        progressBar2.setProgress(progress2);
 
+        progressBar3 = findViewById(R.id.ProgressBar3);
+        progressBar3.setMax(MaxProgressBar3);
+        progressBar3.setProgress(progress3);
+
+        progressBar4 = findViewById(R.id.ProgressBar4);
+        progressBar4.setMax(MaxProgressBar4);
+        progressBar4.setProgress(progress4);
+
+        progressBar5 = findViewById(R.id.ProgressBar5);
+        progressBar5.setMax(MaxProgressBar5);
+        progressBar5.setProgress(progress5);
+
+        progressBar6 = findViewById(R.id.ProgressBar6);
+        progressBar6.setMax(MaxProgressBar6);
+        progressBar6.setProgress(progress6);
+
+        progressBar7 = findViewById(R.id.ProgressBar7);
+        progressBar7.setMax(MaxProgressBar7);
+        progressBar7.setProgress(progress7);
+
+        progressBar8 = findViewById(R.id.ProgressBar8);
+        progressBar8.setMax(MaxProgressBar8);
+        progressBar8.setProgress(progress8);
+
+         */
         // Initialize the back button and set the click listener
         ImageButton backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +103,7 @@ public class BadgesActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -78,17 +147,88 @@ public class BadgesActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
-
     private void CompleteWord()
     {
-
+        if(progress1 < MaxProgressBar1)
+        {
+            progress1 +=1;
+            progressBar1.setProgress(progress1);
+            showBadgeEarned("First word Level 1!!");
+        }
+        if(progress2 < MaxProgressBar2 && progress1 >= MaxProgressBar1)
+        {
+            progress2 +=1;
+            progressBar2.setProgress(progress2);
+            showBadgeEarned("First word Level 2!!");
+        }
+        if(progress3 < MaxProgressBar3 && progress2 >= MaxProgressBar2)
+        {
+            progress3 +=1;
+            progressBar3.setProgress(progress3);
+            showBadgeEarned("First word Level 3!!");
+        }
+        if(progress4 < MaxProgressBar4)
+        {
+            progress4 +=1;
+            progressBar4.setProgress(progress4);
+            showBadgeEarned("Daily Streak!!");
+        }
+        if(progress5 < MaxProgressBar5)
+        {
+            progress5 +=1;
+            progressBar5.setProgress(progress5);
+            if(progress5 == MaxProgressBar5)
+            {
+                showBadgeEarned("10 Word Streak");
+            }
+        }
+        if(progress6 < MaxProgressBar6)
+        {
+            progress6 +=1;
+            progressBar6.setProgress(progress6);
+            if(progress6 == MaxProgressBar6)
+            {
+                showBadgeEarned("20 Word Streak");
+            }
+        }
+        if(progress7 < MaxProgressBar7)
+        {
+            progress7 +=1;
+            progressBar7.setProgress(progress7);
+            if(progress7 == MaxProgressBar7)
+            {
+                showBadgeEarned("50 Word Streak");
+            }
+        }
+        if(progress8 < MaxProgressBar8)
+        {
+            progress8 +=1;
+            progressBar8.setProgress(progress8);
+            if(progress8 == MaxProgressBar8)
+            {
+                showBadgeEarned("100 Word Streak");
+            }
+        }
     }
-
-    private void checkForbadge()
+    // Come back too double check work
+    private void showBadgeEarned(String s)
     {
+        Toast.makeText(this,"you Have earned "+s,Toast.LENGTH_SHORT).show();
+    }
+    private void Loadbadge()
+    {
+        /*
+        progress1 =
+                progress2 =
+        progress3 =
+                progress4 =
+        progress5 =
+                progress6 =
+        progress7 =
+                progress8 =
 
+         */
     }
 
 }
